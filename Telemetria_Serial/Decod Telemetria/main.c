c6af3d93c787c4e996a638f06c1bbc651e33b1c1
Eduardo <eduardo.ime12@gmail.com>
2018-12-04 16:50:16 -0200

    Ajustes e comentários no programa de 'decod telemetria'

--- Telemetria_Serial/Decod Telemetria/main.c
+++ Telemetria_Serial/Decod Telemetria/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include "funcoes.h"
 
+
+/****************************************************************** 
+ * Criação de uma UNION no sentido de compartilhar o mesmo espaço
+ * de memória para dados do tipo 'float' e 'int' com estruturas
+ * do tipo 'unsigned char'. Em cima dessas estruturas que eu 
+ * faço o shift de bits de forma a caber todos os dados que chegam
+ * separados por nibble em um único unsigned char 
+ *
+ * Eduardo H. Santos.
+ * 04/12/2018
+ ******************************************************************/
 typedef union{
     float d_f;
     unsigned char d_c[sizeof(float)];
@@ -10,47 +22,27 @@ typedef union{
     unsigned char d_c[sizeof(float)];
 } int_d;
 
-unsigned char CharToHex( char c )
-{
-    switch( c ) {
-        case '0': return 0;
-        case '1': return 1;
-        case '2': return 2;
-        case '3': return 3;
-        case '4': return 4;
-        case '5': return 5;
-        case '6': return 6;
-        case '7': return 7;
-        case '8': return 8;
-        case '9': return 9;
-        case 'a': case 'A': return 10;
-        case 'b': case 'B': return 11;
-        case 'c': case 'C': return 12;
-        case 'd': case 'D': return 13;
-        case 'e': case 'E': return 14;
-        case 'f': case 'F': return 15;
-    }
-}
 
 int main(void){
-
-    /* Arquivo de leitura */
+	
+    /************************************************************/
+    /* Leitura de arquivo '*.txt' */
     FILE *p_read;
 	p_read = fopen("mydata.txt", "r");
 
-    if(!p_read){
-        printf("Arquivo 'mydata.txt' aberto com sucesso\n");
-        printf("-----------------------------------------\n\n");
+    if ( !p_read ){
+        printf("Arquivo 'mydata.txt' não pode ser aberto.\n");
     }
 
-    /* Arquivo de escrita */
+    /* Escrita em arquivo '*.txt' */
     FILE *p_write;
     p_write = fopen("saida.txt","w");
 
     if(!p_write){
-        printf("Arquivo 'saida.txt' criado com sucesso\n");
-        printf("-----------------------------------------\n\n");
+        printf("Arquivo 'saida.txt' não pode ser criado.\n");
     }
+    /************************************************************/
+
 
     char flag;
     int  i = 0;
@@ -59,6 +51,7 @@ int main(void){
     int_d   inteiro;
 
     do{
+	/* pega caracter a caracter */
         flag = fgetc(p_read);
         if (flag == ' '){
         }
@@ -67,17 +60,22 @@ int main(void){
             i++;
         }  
         if ( i == 12){
-            inteiro.d_c[0] = (buffer[0] << 4) | buffer[1];
-            inteiro.d_c[1] = (buffer[2] << 4) | buffer[3];
+            inteiro.d_c[0]   = (buffer[0] << 4) | buffer[1];
+            inteiro.d_c[1]   = (buffer[2] << 4) | buffer[3];
     
             flutuante.d_c[0] = (buffer[ 4] << 4) | buffer[ 5];
             flutuante.d_c[1] = (buffer[ 6] << 4) | buffer[ 7];
             flutuante.d_c[2] = (buffer[ 8] << 4) | buffer[ 9];
             flutuante.d_c[3] = (buffer[10] << 4) | buffer[11];
 
-            fprintf(p_write, "%d  ",  inteiro.d_i);
-            fprintf(p_write, "%f\n",  flutuante.d_f);
+	    /* Fiz a justaposição dos números à direita.	
+	       No caso eu deixeo um único espaço entre eles,
+	       no caso de aparecer algum número negativo */
+            fprintf(p_write, "%5d",  inteiro.d_i);
+            fprintf(p_write, "%10f\n",  flutuante.d_f);
         }
+
+	/* Se tiver a pulada de linha, eu começo tudo novamente */
         if (flag == '\n'){
             i = 0;
         }
